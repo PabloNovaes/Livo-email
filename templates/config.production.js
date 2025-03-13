d8ce5e83af58201a8ddce454ab4e9758d7663712
Pablo Novaes <ppablo.0028922@gmail.com>
2025-03-13 16:15:18 -0300

    feat: add occurence email

--- templates/config.production.js
+++ templates/config.production.js
@@ -18,6 +18,6 @@ export default {
   },
   css: {
     inline: true,
-    purge: true
+
   }
 };
--- templates/tailwind.config.js
+++ templates/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   presets: [
     require('tailwindcss-preset-email'),
   ],
+  safelist: [
+    { pattern: /^\[data-status="(PENDENTE|CONCLUIDA|EM ANDAMENTO|INFERIDO|CANCELADO)"\]$/, }
+  ],
   content: [
     './components/**/*.html',
     './emails/**/*.html',
